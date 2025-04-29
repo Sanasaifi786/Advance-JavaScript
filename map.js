df82638c7ee8009b03f0f686a8d9ee12df6f3596
Sanasaifi786 <sanasaifi1420@gmail.com>
2025-04-29 12:59:42 +0530

    Destructuring of strings and Map, Trailing Commas

--- map.js
+++ map.js
@@ -94,10 +94,78 @@
 // console.log(country);
 
 //Alias
-const person={
-    firstName: "Sana",
-    lastName: "Saifi",
-    age:20
-};
-let{lastName: name}=person;
-console.log(name);
+// const person={
+//     firstName: "Sana",
+//     lastName: "Saifi",
+//     age:20
+// };
+// let{lastName: name}=person;
+// console.log(name);
+
+//String Destructuring
+// let name="GurukulTheSchools";
+// let[a1,a2,a3,a4,a5]=name;
+// console.log(a1,a2,a3,a4,a5);
+
+// const fruits=["Bananas","Oranges","Apples","Mangoes"];
+//let[fruits1,fruits2]=fruits;
+// console.log(fruits1,fruits2);
+// let[fruits1,,,fruits2]=fruits;
+// console.log(fruits1,fruits2);
+// let{[0]:fruits1,[3]:fruits2}=fruits;
+// console.log(fruits1,fruits2);
+
+//Rest property=store all remaining value into new array
+// const numbers=[10,20,30,40,50,60,70];
+// const[a,b,...rest]=numbers;
+// console.log(a,b,...rest);
+
+//Destrcturing in Map
+// const fruits=new Map([
+//     ["apples",500],
+//     ["bananas",300],
+//     ["oranges",200]
+// ]);
+
+//swaping the two elements
+// let firstName="sana";
+// let lastName="saifi";
+// [firstName,lastName]=[lastName,firstName];
+// console.log(firstName,lastName);
+
+//JavaScript Exponential
+// let x=5;
+// let z=x**2
+// console.log(z);
+// let p=Math.pow(x,2);
+// console.log(p);
+// console.log(x**=2);
+
+//Array includes()=it gives boolean value
+// const fruits=["Bananas","Oranges","Apples","Mangoes"];
+// console.log(fruits.includes("Mangoes"));
+// console.log(fruits.includes("Bananas",3));
+
+//Trailing commas=it gives the teminal point and only use one comma
+// const arr=[
+//     "one",
+//     "two",
+//     "three",,
+// ];
+// console.log(arr);
+// console.log(arr.length);
+
+// const person={
+//     firstName: "Sana",
+//     lastName: "Saifi",
+//     age:20,
+// }
+// console.log(person);
+
+//example of sparsh matrices
+// const person={
+//     firstName: "Sana",
+//     ,
+//     age:20,
+// }
+// console.log(person);
